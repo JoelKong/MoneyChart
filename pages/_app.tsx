b621118b7460e9df52ba216ea3db959f16784150
Joel Kong <joelkong2001@gmail.com>
2023-01-09 20:51:56 +0800

    feat: added global header for moneychart

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import "../styles/globals.css";
+
+import type { AppProps } from "next/app";
+import { Fragment } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <title>MoneyChart</title>
+        <meta name="description" content="Visualising data through charts" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
