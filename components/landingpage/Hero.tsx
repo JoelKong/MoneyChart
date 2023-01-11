81957edef25237ee09614073f36f92685bf2e469
Joel Kong <joelkong2001@gmail.com>
2023-01-11 14:13:18 +0800

    chore: setup hero page

--- components/landingpage/Hero.tsx
+++ components/landingpage/Hero.tsx
@@ -1,17 +1,23 @@
-import { Fragment } from "react";
 import Image from "next/image";
 import classes from "./hero.module.css";
 
 function Hero() {
   return (
-    <Fragment>
+    <div className={classes.center}>
       <Image
         src="/images/moneychart-brand.png"
         alt="MoneyChart Logo"
         width={500}
         height={200}
       />
-    </Fragment>
+      <h1 className={classes.slogan}>
+        Tracking Finances Has Never Been Easier
+      </h1>
+      <div className={classes.actions}>
+        <button>Log In</button>
+        <button>More Details</button>
+      </div>
+    </div>
   );
 }
 
