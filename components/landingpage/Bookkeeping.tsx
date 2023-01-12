89ab13a6f35d57600d8fb9123a85a4ca43f387c3
Joel Kong <joelkong2001@gmail.com>
2023-01-12 21:47:25 +0800

    chore: implemented loading component only on scroll

--- components/landingpage/Bookkeeping.tsx
+++ components/landingpage/Bookkeeping.tsx
@@ -1,11 +1,43 @@
+import { useState, useEffect } from "react";
 import classes from "./bookkeeping.module.css";
 
 function Bookkeeping() {
-  return (
-    <article className={classes.bookkeeping}>
-      <p>sdfsd</p>
-    </article>
-  );
+  const [isComponentMounted, setIsComponentMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const threshold: number = window.innerHeight * 0.2;
+      if (window.pageYOffset > threshold) {
+        setIsComponentMounted(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return (): void => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  if (!isComponentMounted) {
+    return null;
+  } else
+    return (
+      <section className={classes.bookkeeping}>
+        <div>PIE CHART</div>
+        <article>
+          <div>
+            <h2>Bookkeeping</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi nam
+              ipsa, earum corrupti explicabo odio sint quis odit? Reiciendis
+              odit, fuga deleniti repellat rem nemo explicabo eligendi ullam
+              quae culpa?
+            </p>
+          </div>
+        </article>
+      </section>
+    );
 }
 
 export default Bookkeeping;
