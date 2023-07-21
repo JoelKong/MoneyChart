9b861c39ccd5fdd517e126598b8d15456aef09f2
Joel Kong <joelkong2001@gmail.com>
2023-07-21 14:57:23 +0800

    feat: created header section

---
+++ moneychart/components/home/Header.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function Header(): JSX.Element {
+  function login() {
+    console.log("g");
+  }
+
+  return (
+    <header className="fixed flex w-full justify-between items-center pl-4 pt-2">
+      <Image
+        src="/moneychartt.png"
+        alt="moneychart"
+        height={50}
+        width={50}
+        className="hover: cursor-pointer"
+      />
+      <button
+        className="mr-12 w-32 h-12 tracking-wide font-semibold rounded-lg transition ease-in-out bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
+        onClick={login}
+      >
+        Log In
+      </button>
+    </header>
+  );
+}
--- moneychart/src/pages/index.tsx
+++ moneychart/src/pages/index.tsx
@@ -1,9 +1,13 @@
 import Hero from "../../components/home/Hero";
+import Header from "../../components/home/Header";
 
 export default function Home() {
   return (
     <>
-      <Hero />
+      <main className="w-screen h-screen fixed bg-gradient-to-br from-[#b6f492] to-[#338b93]">
+        <Header />
+        <Hero />
+      </main>
     </>
   );
 }
