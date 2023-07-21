0af28f5bb88c87828c57a2fc4fd2dad9d883955b
Joel Kong <joelkong2001@gmail.com>
2023-07-21 15:39:05 +0800

    feat: made scroll snap feature

---
+++ moneychart/components/home/Bookkeeping.tsx
@@ -0,0 +1,7 @@
+export default function Bookkeeping(): JSX.Element {
+  return (
+    <div className="flex justify-center items-center w-full h-full">
+      gfffffffffffffffffffffffi
+    </div>
+  );
+}
--- moneychart/src/pages/index.tsx
+++ moneychart/src/pages/index.tsx
@@ -1,12 +1,18 @@
 import Hero from "../../components/home/Hero";
 import Header from "../../components/home/Header";
+import Bookkeeping from "../../components/home/Bookkeeping";
 
 export default function Home() {
   return (
     <>
-      <main className="w-screen h-screen fixed bg-gradient-to-br from-[#b6f492] to-[#338b93]">
-        <Header />
-        <Hero />
+      <main className="w-screen h-screen fixed bg-gradient-to-br from-[#b6f492] to-[#338b93] overflow-y-scroll snap-y snap-mandatory">
+        <div className="snap-center w-full h-full">
+          <Header />
+          <Hero />
+        </div>
+        <div className="snap-center w-full h-full">
+          <Bookkeeping />
+        </div>
       </main>
     </>
   );
