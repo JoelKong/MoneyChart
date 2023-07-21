fcaea20a21b061d29cf08ecf4cec04cbc76c2d15
Joel Kong <joelkong2001@gmail.com>
2023-07-21 16:25:16 +0800

    feat: added bookkeeping section

--- moneychart/components/home/Bookkeeping.tsx
+++ moneychart/components/home/Bookkeeping.tsx
@@ -1,7 +1,26 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+import { samplePieData } from "../../utils/samplechart";
+
 export default function Bookkeeping(): JSX.Element {
+  ChartJS.register(ArcElement, Tooltip, Legend);
+
   return (
-    <div className="flex justify-center items-center w-full h-full">
-      gfffffffffffffffffffffffi
-    </div>
+    <section className="flex justify-around items-center w-full h-full">
+      <div>
+        <Pie data={samplePieData} width={400} height={400} />
+      </div>
+      <article className="w-1/4 h-2/4">
+        <div className="flex w-full h-full flex-col items-center">
+          <h2 className="text-3xl font-bold underline">Bookkeeping</h2>
+          <p className="pt-6 tracking-wide text-xl font-medium leading-10">
+            Visualise your expenses easily through MoneyChart. We deliver charts
+            which are categorised based off your transactions for your needs.
+            Keeping track of your spending would never again be a hassle when
+            you sign up with us.
+          </p>
+        </div>
+      </article>
+    </section>
   );
 }
---
+++ moneychart/utils/samplechart.ts
@@ -0,0 +1,15 @@
+export const samplePieData = {
+  labels: ["Grocery", "Food", "Entertainment", "Investment"],
+  datasets: [
+    {
+      label: "Amount",
+      data: [100, 300, 50, 700],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+      ],
+    },
+  ],
+};
