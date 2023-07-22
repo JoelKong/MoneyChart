b1a6f523d00a13adb5c6af4fb10f28fde0c01a91
Joel Kong <joelkong2001@gmail.com>
2023-07-22 21:38:01 +0800

    fix: redirect back to dashboard with token attached

--- moneychart/src/pages/api/accesstoken.ts
+++ moneychart/src/pages/api/accesstoken.ts
@@ -4,24 +4,27 @@ async function handler(req: any, res: any) {
   const sandboxClientID = "c871240c-d971-4b74-9b13-4be5b3a27bd1";
   const sandboxClientSecret = "62813690-4c56-4e4c-8634-3f232edf516c";
   const authCode = btoa(`${sandboxClientID}:${sandboxClientSecret}`); //https://www.dbs.com/developers/#/all-products/play-ground
-  let redirectURI = 'http://localhost:3000/api/accesstoken'
+  let redirectURI = "http://localhost:3000/api/accesstoken";
 
   let fetchToken = {
-    method: 'POST',
+    method: "POST",
     body: `code=${code}&grant_type=authorization_code&redirect_uri=${redirectURI}`,
     headers: {
-        'authorization': `Basic ${authCode}`,
-        'content-type': 'application/x-www-form-urlencoded',
-        'cache-control': 'no-cache',
-        'accept': 'application/json'
-    }
-  }
-  let response = await fetch('https://www.dbs.com/sandbox/api/sg/v1/oauth/tokens', fetchToken)
-
+      authorization: `Basic ${authCode}`,
+      "content-type": "application/x-www-form-urlencoded",
+      "cache-control": "no-cache",
+      accept: "application/json",
+    },
+  };
+  let response = await fetch(
+    "https://www.dbs.com/sandbox/api/sg/v1/oauth/tokens",
+    fetchToken
+  );
 
   let parsed = await response.json();
-  console.log(parsed);
-  res.status(200).json({ parsed });
+  res.redirect(
+    `http://localhost:3000/dashboard?access_token=${parsed.access_token}`
+  );
 }
 
 export default handler;
--- moneychart/src/pages/dashboard/index.tsx
+++ moneychart/src/pages/dashboard/index.tsx
@@ -1,10 +1,10 @@
 import Hero from "../../../components/home/Hero";
 import Header from "../../../components/home/Header";
 import Bookkeeping from "../../../components/home/Bookkeeping";
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { useRouter } from "next/router";
 
-export default function Home({ param }: any) {
+export default function Home({ accessToken }: any) {
   const router = useRouter();
   const moreDetailsRef = useRef<any>();
 
@@ -16,41 +16,6 @@ export default function Home({ param }: any) {
     });
   }
 
-  async function getAccessToken() {
-    const sandboxClientID = "c871240c-d971-4b74-9b13-4be5b3a27bd1";
-    const sandboxClientSecret = "62813690-4c56-4e4c-8634-3f232edf516c";
-    const authCode = btoa(`${sandboxClientID}:${sandboxClientSecret}`);
-    const redirectURL = "localhost:3000/dashboard";  //https://www.dbs.com/developers/#/all-products/play-ground
-    const code:any = router.query.code;
-    const grantType = 'token'
-    const authHeader = `Basic ${authCode}`
-    const requestBody = `code=${encodeURIComponent(code)}&redirect_uri=${encodeURIComponent(redirectURL)}&grant_type=${encodeURIComponent(grantType)}`
-    // let fetchToken = {
-    //   method: "POST",
-    //   body: `code=${code}&grant_type=authorization_code&redirect_uri=${redirectURL}`,
-    //   headers: {
-    //     'authorization' : `Basic ${authCode}`,
-    //     'content-type': 'application/x-www-form-urlencoded',
-    //     'cache-control': 'no-cache',
-    //     'accept': 'application/json',
-    //   },
-    // };
-    let response = await fetch(
-      "https://www.dbs.com/sandbox/api/sg/v1/oauth/tokens", {
-        method: 'POST',
-        headers: {
-          'Authorization': authHeader,
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: requestBody
-      }
-    );
-    let parsed = await response.json()
-    console.log(parsed)
-
-    return;
-  }
-
   return (
     <>
       <main
@@ -58,9 +23,6 @@ export default function Home({ param }: any) {
         ref={moreDetailsRef}
       >
         <div className="snap-center w-full h-full">
-          <button className="w-12 h-12 border-2" onClick={getAccessToken}>
-            Get token
-          </button>
           <Header />
           <Hero moreDetails={moreDetails} />
         </div>
